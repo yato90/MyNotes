4aed5680a9615bdbd17e2fa6bf358be92e28ec47
yato90 <zahaoyam@yandex.ru>
2025-01-31 22:59:12 +0300

    fix small

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,5 +1,4 @@
 import './styles/style.css'
-import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/MyNotes/',
-  build: {
-    outDir: 'dist'
-  }
+  base: '/MyNotes/'
 })
