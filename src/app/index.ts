68a25ea59a31c416e72b0fa828f251f1f5e49b0b
yato90 <zahaoyam@yandex.ru>
2025-01-31 23:28:24 +0300

    fix

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,6 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 
-const app = createApp(App).use(createPinia()).use(router)
+const app = createApp(App).use(router)
 
 export default app
